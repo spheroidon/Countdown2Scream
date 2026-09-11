3c10f92e15a0d43b9949dac76171b527d228f9c5
Spheroidon <spheroidon@gmail.com>
2026-09-11 22:20:08 +0300

    Added status text

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 Press a button to start a random hidden countdown. Once the countdown is over, the site will scream at you.
 
+Written in pure HTML/JS/CSS.
+
 ## Features:
 - Countdown
 - Screaming
-
-## Todo:
-- Clock audio while the countdown is still going
--- script.js
+++ script.js
@@ -2,22 +2,39 @@ let waiting = false;
 let waitTime = 0;
 let screaming = false;
 
+const text = document.getElementById("txt");
+const idleText = "Press anywhere to start the countdown.";
+const screamText = "PRESS ANYWHERE TO MAKE IT STOP";
+const startedText = "Countdown started. Be patient.";
+
 const minTime = 2;
 const maxTime = 50;
 
 const screamAudio = new Audio("assets/scream.mp3");
 screamAudio.loop = true;
 
+const tickingAudio = new Audio("assets/ticking.mp3");
+tickingAudio.loop = true;
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    text.innerText = idleText;
+})
+
 document.addEventListener("click", function (e) {
     if (!screaming) {
         if (!waiting) {
+            text.innerText = startedText;
             waiting = true;
             waitTime = Math.random() * (maxTime - minTime) + minTime;
+            tickingAudio.play();
             console.log("Countdown seconds: " + waitTime);
             setTimeout(function () {
                 waiting = false;
                 waitTime = 0;
                 screaming = true;
+                tickingAudio.pause();
+                text.innerText = screamText;
+                tickingAudio.currentTime = 0;
                 screamAudio.play();
                 document.body.classList.add("scream");
                 console.log("Countdown ended! Scream!");
@@ -27,5 +44,6 @@ document.addEventListener("click", function (e) {
         screaming = false;
         screamAudio.pause();
         document.body.classList.remove("scream");
+        text.innerText = idleText;
     }
 })
