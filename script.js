432fd0b6081682317a6fcc6b6c3d57c84ffc2bda
Spheroidon <spheroidon@gmail.com>
2026-09-22 11:56:16 +0300

    Impatience

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 let waiting = false;
 let waitTime = 0;
 let screaming = false;
+let impatient = false;
 
 const minTime = 1;
 const maxTime = 90;
+
+const impatienceTime = 1;
 
 // Text variables
 const text = document.getElementById("txt");
 const idleText = "Press anywhere to start the countdown.";
 const screamText = "PRESS ANYWHERE TO MAKE IT STOP";
-const startedText = "Countdown started. Be patient.";
+const waitingText = "Countdown started. Be patient.";
+const impatientText = "BE PATIENT";
 
 // Audio variables
 const screamAudio = new Audio("assets/scream.mp3");
@@ -18,6 +22,8 @@ screamAudio.loop = true;
 
 const tickingAudio = new Audio("assets/ticking.mp3");
 tickingAudio.loop = true;
+
+const impatientAudio = new Audio("assets/impatient.mp3");
 
 // Wakelock functionality
 let wakeLock = null;
@@ -41,7 +47,7 @@ function handleSite(e) {
     if (!screaming) {
         if (!waiting) {
             // Start countdown
-            text.innerText = startedText;
+            text.innerText = waitingText;
             waiting = true;
             waitTime = Math.random() * (maxTime - minTime) + minTime;
             tickingAudio.play();
@@ -58,9 +64,29 @@ function handleSite(e) {
                 text.innerText = screamText;
                 tickingAudio.currentTime = 0;
                 screamAudio.play();
+                document.body.classList.remove("impatient");
                 document.body.classList.add("scream");
                 console.log("Countdown ended! Scream!");
             }, waitTime * 1000)
+        } else {
+            if (!impatient) {
+                // Impatient
+                impatient = true;
+                text.innerText = impatientText;
+                tickingAudio.pause();
+                impatientAudio.currentTime = 0;
+                impatientAudio.play();
+                document.body.classList.add("impatient");
+                setTimeout(function () {
+                    if (waiting) {
+                        // Restore waiting state
+                        impatient = false;
+                        document.body.classList.remove("impatient");
+                        text.innerText = waitingText;
+                        tickingAudio.play();
+                    }
+                }, impatienceTime * 1000);
+            }
         }
     } else {
         // Stop screaming
